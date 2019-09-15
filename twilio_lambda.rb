20279239b8be477a8ac297536052eb9156b3de13
guayosr <guayosr@gmail.com>
2019-09-15 22:23:29 +0000

    text forwarding done

--- twilio_lambda.rb
+++ twilio_lambda.rb
@@ -1,40 +1,54 @@
 require 'json'
 require 'twilio-ruby'
 
-account_sid = ENV['twilio_account_sid']
-auth_token = ENV['twilio_auth_token']
+def hello_world(event:,context:)
+	# set up a client to talk to the Twilio REST API
+	account_sid = ENV['twilio_account_sid']
+	auth_token = ENV['twilio_auth_token']
+	@client = Twilio::REST::Client.new account_sid, auth_token
+	
+	#to-do: figure out how to parse this to drop weird characters
+	text_body = "From: " + 
+		event['From'] + 
+		"\n Text:" + 
+		event['Body']
+	
+	@client.api.account.messages.create(
+	  from: ENV['from_number'],
+	  to: ENV['to_number'],
+	  body: text_body
+	)
+end
 
-# set up a client to talk to the Twilio REST API
-@client = Twilio::REST::Client.new account_sid, auth_token
+def forward_message(event:,context:)
+	# set up a client to talk to the Twilio REST API
+	account_sid = ENV['twilio_account_sid']
+	auth_token = ENV['twilio_auth_token']
+	@client = Twilio::REST::Client.new account_sid, auth_token
+	
+	#to-do: figure out how to parse this to drop weird characters
+	text_body = "From: " + 
+		event['From'] + 
+		"\n Text:" + 
+		event['Body']
+	
+	@client.api.account.messages.create(
+	  from: ENV['from_number'],
+	  to: ENV['to_number'],
+	  body: text_body
+	)
+end
 
-def hello_world(event)
-	#puts "ENV variable"
-	#puts ENV['TEST']
-	#puts "This is the end of the event"
 	
-	#puts "Received event:"
-	#puts event
-	#puts "This is the end of the event"
-	
-	builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-	    xml.Response {
-	      xml.Message "Hello world! - Lambda w XML"
-	    }
-	end
-	return builder.to_xml
-	
-	#standard Lambda rest response
-	#{ statusCode: 200, body: JSON.generate('Hello from Twilioland') }
+	#builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+	#    xml.Response {
+	#      xml.Message "Hello world! - Lambda w XML"
+	#    }
+	#end
+	#return builder.to_xml
 	
 	# @client.api.account.calls.create(
 	#   from: '+12487600161',
 	#   to: '+525580366674',
 	#   url: 'http://example.com'
 	# )
-
-	# @client.api.account.messages.create(
-	#   from: '+12487600161',
-	#   to: '+525580366674',
-	#   body: 'HOLA MUNDO'
-	# )
-end
