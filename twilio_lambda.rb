f17ef53c58d39e4799da75379327d0066d288eb5
guayosr <guayosr@gmail.com>
2019-09-16 16:24:30 +0000

    added call handling

--- twilio_lambda.rb
+++ twilio_lambda.rb
@@ -24,53 +24,42 @@ end
 
 
 def receive_and_record_call(event:,context:)
-	# set up a client to talk to the Twilio REST API
-	# account_sid = ENV['twilio_account_sid']
-	# auth_token = ENV['twilio_auth_token']
-	# @client = Twilio::REST::Client.new account_sid, auth_token
+	#set up a client to talk to the Twilio REST API
+	account_sid = ENV['twilio_account_sid']
+	auth_token = ENV['twilio_auth_token']
+	@client = Twilio::REST::Client.new account_sid, auth_token
 	
-	# #1/ receive a call and send a text notifying who called
-	# puts event
-	
-	# text_body = "Call from: " + event['From'] 
-	
-	# @client.api.account.messages.create(
-	#   from: ENV['from_number'],
-	#   to: ENV['to_number'],
-	#   body: text_body
-	# )
-	
-	#2/ record call and find easiest was to send output
+	puts "Processing event:"
 	puts event
 	
-	recording = Twilio::TwiML::VoiceResponse.new do |r|
-	    # Use <Say> to give the caller some instructions
-	    r.say(message: 'Hello. Please leave a message after the beep.')
+	case event['CallStatus']
+		when'ringing'
+			puts "Processing ring:"
+			response = Twilio::TwiML::VoiceResponse.new do |r|
+			    # Use <Say> to give the caller some instructions
+			    # to-do: different message based on MX vs US
+			    r.say(message: 'Hello. Please leave a message after the beep.')
+			
+			    # Use <Record> to record the caller's message
+			    r.record(transcribe: true)
+			
+			    # End the call with <Hangup>
+			    r.hangup
+			 end.to_s
+			 puts "Finished recording"
+		when 'completed'
+			puts "Processing completion:"
+			text_body = "You had a call from: " +
+				event['From'] +
+				"\n Audio Recording:" +
+				CGI.unescape(event['RecordingUrl'])
 	
-	    # Use <Record> to record the caller's message
-	    r.record(transcribe: true)
-	
-	    # End the call with <Hangup>
-	    r.hangup
-	 end.to_s
-	 
-	 #puts recording
-	
-	#3/ record call and transcribe, puts it
-	#4/ record call, transcribe, send it via email or text
-	
+			@client.api.account.messages.create(
+			  from: ENV['from_number'],
+			  to: ENV['to_number'],
+			  body: text_body,
+			  #media_url: ['https://img.etimg.com/thumb/msid-68738133,width-643,imgsize-70881,resizemode-4/twittersized.jpg']
+			)
+	end
+	response	 
 end
-
-	
-	#builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-	#    xml.Response {
-	#      xml.Message "Hello world! - Lambda w XML"
-	#    }
-	#end
-	#return builder.to_xml
-	
-	# @client.api.account.calls.create(
-	#   from: '+12487600161',
-	#   to: '+525580366674',
-	#   url: 'http://example.com'
-	# )
